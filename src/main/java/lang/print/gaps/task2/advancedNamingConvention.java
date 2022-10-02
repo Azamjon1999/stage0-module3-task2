796b2449fa9890925de007b920b51ed7625e2379
Azamjon1999 <114771816+Azamjon1999@users.noreply.github.com>
2022-10-02 11:49:16 +0500

    Update advancedNamingConvention.java

--- src/main/java/lang/print/gaps/task2/advancedNamingConvention.java
+++ src/main/java/lang/print/gaps/task2/advancedNamingConvention.java
@@ -2,13 +2,9 @@ package lang.print.gaps.task2;
 
 public class AdvancedNamingConvention {
     public static void main(String[] args){
-    
-    }
-    private static final int ADULT_AGE = 18;
-
-    private int age;
-    private int phoneNumber;
-
+    final int ADULT_AGE = 18;
+         int age, phoneNumber;
+ }
    public static void callToFriend() {
         //some code
     }
